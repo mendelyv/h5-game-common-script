5f59994093bd3946cddbb1adce9ae4167b308f27
yanghaoran <yanghaoran@qushen.net>
2022-08-01 15:39:34 +0800

    界面基类增加widget适配队列

--- cocos creator/view/BaseView.ts
+++ cocos creator/view/BaseView.ts
@@ -15,7 +15,8 @@ export default class BaseView extends cc.Component {
     public static LAYER: ContainerType;
     public static prefabPath: string;
     public prefabName: string;
-    public adapterNodes: cc.Node[] = [];
+    public adapterScaleNodes: cc.Node[] = [];
+    public adapterWidgetNodes: cc.Node[] = [];
     start() {
         this.screenAdapter();
     }
@@ -45,18 +46,31 @@ export default class BaseView extends cc.Component {
     }
     public static getContainer(): cc.Node { return null; }
     public screenAdapter() {
-        if(this.adapterNodes.length < 0) return;
+        if(this.adapterScaleNodes.length < 0) return;
         if(!this.needAdapter()) return;
 
         let designHeight = cc.view.getDesignResolutionSize().height;
         let screenHeight = cc.winSize.height;
 
-        for(let i = 0; i < this.adapterNodes.length; i++) {
-            let node = this.adapterNodes[i];
+        for(let i = 0; i < this.adapterScaleNodes.length; i++) {
+            let node = this.adapterScaleNodes[i];
             let scale = node.scale;
             node.scale = scale * screenHeight / designHeight;
         }
-        this.adapterNodes = [];
+        this.adapterScaleNodes = [];
+
+        for(let i = 0; i < this.adapterWidgetNodes.length; i++) {
+            let node = this.adapterWidgetNodes[i];
+            let widget = node.getComponent(cc.Widget);
+            if(!widget) continue;
+            if(widget.left) widget.left = widget.left * screenHeight / designHeight;
+            if(widget.right) widget.right = widget.right * screenHeight / designHeight;
+            if(widget.top) widget.top = widget.top * screenHeight / designHeight;
+            if(widget.bottom) widget.bottom = widget.bottom * screenHeight / designHeight;
+            if(widget.horizontalCenter) widget.horizontalCenter = widget.horizontalCenter * screenHeight / designHeight;
+            if(widget.verticalCenter) widget.verticalCenter = widget.verticalCenter * screenHeight / designHeight;
+        }
+        this.adapterWidgetNodes = [];
     }
     protected needAdapter() {
         let designHeight = cc.view.getDesignResolutionSize().height;
